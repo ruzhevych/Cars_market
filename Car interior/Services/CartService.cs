c598789f1c9235547ddc8fb5da19be4c8555241c
ruzhevych.veniamin@gmail.com <ruzhevych.veniamin@gmail.com>
2024-08-16 19:06:33 +0300

    Move all cart logic to cart service

--- Car interior/Services/CartService.cs
+++ Car interior/Services/CartService.cs
@@ -1,15 +1,30 @@
 ﻿﻿using AutoMapper;
 using Core.Dtos;
+using Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Car_market.Extensions;
 
 namespace Car_market.Services
 {
+    //public interface ICartService
+    //{
+    //    int GetCount();
+    //    void AddItem(int id);
+    //    void RemoveItem(int id);
+    //    List<ProductDto> GetProducts();
+    //}
+
     public class CartService
     {
         private readonly HttpContext httpContext;
-        public CartService(IHttpContextAccessor contextAccessor)
+        private readonly IMapper mapper;
+        private readonly CarsDbContext context;
+        public CartService(IHttpContextAccessor contextAccessor, IMapper mapper, CarsDbContext context)
         {
             httpContext = contextAccessor.HttpContext!;
+            this.mapper = mapper;
+            this.context = context;
         }
 
         public int GetCount()
@@ -19,6 +34,37 @@ namespace Car_market.Services
             if (ids == null) return 0;
 
             return ids.Distinct().Count();
+        }
+
+        public List<CarsDto> GetProducts() 
+        {
+            var ids = httpContext.Session.Get<List<int>>("cart_items") ?? new();
+
+            var products = context.Cars.Include(x => x.Category).Where(x => ids.Contains(x.Id)).ToList();
+
+            return mapper.Map<List<CarsDto>>(products);
+        }
+
+        public void AddItem(int id)
+        {
+            var ids = httpContext.Session.Get<List<int>>("cart_items");
+
+            if (ids == null) ids = new();
+
+            ids.Add(id);
+
+            httpContext.Session.Set("cart_items", ids);
+        }
+
+        public void RemoveItem(int id)
+        {
+            var ids = httpContext.Session.Get<List<int>>("cart_items");
+
+            if (ids == null || !ids.Contains(id)) return; // throw exception
+
+            ids.Remove(id);
+
+            httpContext.Session.Set("cart_items", ids);
         }
     }
 }
